34cf9a21c4ef0539df8b851e4342f7301c9ddacf
Listochek <67066611+Listochek@users.noreply.github.com>
2024-07-26 19:30:54 +0700

    add discription  to func check_images

--- tests/main_test.py
+++ tests/main_test.py
@@ -3,5 +3,6 @@ import os
 
 
 def check_images(number_of_pictures: int, save_dir: str, folder_name: str = 'Crawler') -> float:
+   '''Test to count how many images were downloaded'''
    pic_cou = len(os.listdir(f'{save_dir}/{folder_name}'))
    return [pic_cou, pic_cou/number_of_pictures * 100]
