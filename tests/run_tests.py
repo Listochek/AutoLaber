9e77e20edd55aa9644f7913da622caad57d4792e
Rt811 <sataev.vl@gmail.com>
2024-07-30 22:14:37 +0700

    add test todelite duplicate picture

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,14 +5,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from code.listcrawl import bing_list_crawler
 
 
-from main_test import check_images, image_weight, process_images_in_directory
+from main_test import check_images, image_weight, process_images_in_directory, find_duplicate_images
 
 
 KEYS = ['котики', 'мышки', 'коты']
 direct = 'picture'
 kef = 5
-#bing_list_crawler(KEYS, direct, kef)
+bing_list_crawler(KEYS, direct, kef)
 
-print(check_images(len(KEYS) * kef, direct))
-print(image_weight(direct))
-print(process_images_in_directory(direct))
+#print(check_images(len(KEYS) * kef, direct))
+#print(image_weight(direct))
+#print(process_images_in_directory(direct))
+#print(find_duplicate_images(direct))
