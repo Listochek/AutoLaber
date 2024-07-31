4e757548e04724aa0b4869477b9831b5a5f225a1
Rt811 <sataev.vl@gmail.com>
2024-07-31 16:23:02 +0700

    finished class RemoveDuplicate

--- code/listcrawl.py
+++ code/listcrawl.py
@@ -35,7 +35,7 @@ class FolderManager:
     def remove_old_folder(parent_dir: str) -> None:
         if os.path.isdir(parent_dir):
             shutil.rmtree(parent_dir)
-            
+
 # в траейн 75% вал 20% в тест 5%
 class FolderSeparation:
     def __init__(self) -> None:
@@ -43,7 +43,7 @@ class FolderSeparation:
 
     def drop_duplicates():
         pass
-    def separation(): # rando - 3e
+    def separation(): # random = True/False
         pass  
     def add_folders(): 
         pass
@@ -66,34 +66,36 @@ class FolderSeparation:
         pass
 
 class RemoveDuplicate():
-    pass
-    '''Remove Duplicates in folders
+    
     def __init__(self) -> None:
         pass
     
-    def find_duplicate_images(folder_path: str, delite_picture : bool = False):
-    # Словарь для хранения хешей изображений
-    hash_dict = defaultdict(list)
-    # Перебираем файлы в папке
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-                # Проверяем, является ли файл изображением (можно добавлять другие форматы)
-            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-                file_path = os.path.join(root, file)
-                    # Создаем хеш изображения
-                image_hash = hash_image(file_path)
-                hash_dict[image_hash].append(file_path)
+    def find_duplicate_images(self, folder_path: str, delite_picture : bool = False):
+        # Словарь для хранения хешей изображений
+        hash_dict = defaultdict(list)
+        # Перебираем файлы в папке
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                    # Проверяем, является ли файл изображением (можно добавлять другие форматы)
+                if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                    file_path = os.path.join(root, file)
+                        # Создаем хеш изображения
+                    image_hash = self.hash_image(file_path)
+                    hash_dict[image_hash].append(file_path)
 
-        # Возвращаем только повторяющиеся изображения
-    duplicates = {hash_value: paths for hash_value, paths in hash_dict.items() if len(paths) > 1}
-    # условие на удаление по повторению delite_picture = True
-    if delite_picture==False:
-        return duplicates
-    else:
-        drop_dublicate(duplicates)
-        return duplicates
+            # Возвращаем только повторяющиеся изображения
+        duplicates = {hash_value: paths for hash_value, paths in hash_dict.items() if len(paths) > 1}
+        # условие на удаление по повторению delite_picture = True
+        if delite_picture==False:
+            print(duplicates)
+            return duplicates
+            
+        else:
+            self.drop_dublicate(duplicates)
+            print(f'{duplicates} - БЫЛИ ДРОПНУТЫ')
+            return duplicates
 
-    def hash_image(file_path) -> str:
+    def hash_image(self, file_path) -> str:
         """Создает хеш файла изображения."""
         hash_md5 = hashlib.md5()
         with open(file_path, "rb") as f:
@@ -101,14 +103,19 @@ class RemoveDuplicate():
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
 
-    def drop_dublicate(duplicates: dict):
-    keys_to_del = duplicates.keys()
-    for i in keys_to_del:
-        massik = duplicates.get(i, None)
-        massik.pop(0)
-        for j in massik:
-            os.remove(j)   
-'''     
+    def drop_dublicate(self, duplicates: dict):
+        keys_to_del = duplicates.keys()
+        for i in keys_to_del:
+            massik = duplicates.get(i, None)
+            massik.pop(0)
+            for j in massik:
+                os.remove(j)   
+
+'''    def process_duplicates(self, ):
+        pass'''
+    
+
+'''
 def bing_list_crawler(key_words: list, Save_path: str, max_pic: int = 1, main_folder_name: str = 'Crawler', synonym: bool = False):
     FolderManager.remove_old_folder(Save_path)
     FolderManager.add_folders(Save_path, key_words)
@@ -117,4 +124,9 @@ def bing_list_crawler(key_words: list, Save_path: str, max_pic: int = 1, main_fo
         bing_crawler = BingImageCrawler(storage={'root_dir': f'{Save_path}/{i}'})
         bing_crawler.crawl(keyword=i, max_num=max_pic, file_idx_offset=0)
     FolderManager.piture_rename(Save_path, main_folder_name, FolderManager.get_all_filenames(Save_path))
-#FolderSeparation.runer()
+#FolderSeparation.runer()'''
+
+duplicate_remover = RemoveDuplicate()
+
+# Вызов метода find_duplicate_images через экземпляр класса
+duplicate_remover.find_duplicate_images(folder_path='picture\Crawler', delite_picture=True)
